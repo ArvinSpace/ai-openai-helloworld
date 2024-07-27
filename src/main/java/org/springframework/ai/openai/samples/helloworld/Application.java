305681966e7b97886370b6519401a9a83266a071
Arvin <yanhui-180@163.com>
2024-07-27 12:49:01 +0800

    fix

--- src/main/java/org/springframework/ai/openai/samples/helloworld/Application.java
+++ src/main/java/org/springframework/ai/openai/samples/helloworld/Application.java
@@ -1,11 +1,11 @@
 package org.springframework.ai.openai.samples.helloworld;
 
-import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class Application {
 
     public static void main(String[] args) {
