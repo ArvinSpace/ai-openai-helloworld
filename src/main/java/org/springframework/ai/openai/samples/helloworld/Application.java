95926ec98240103c94f1907328cc9d3e072195ca
Arvin <yanhui-180@163.com>
2024-07-27 13:10:07 +0800

    proxy set

--- src/main/java/org/springframework/ai/openai/samples/helloworld/Application.java
+++ src/main/java/org/springframework/ai/openai/samples/helloworld/Application.java
@@ -9,6 +9,16 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
 
     public static void main(String[] args) {
+        String proxyHost = "127.0.0.1";
+        //
+        String proxyPort = "7890";
+        // 对http开启代理
+        System.setProperty("http.proxyHost", proxyHost);
+        System.setProperty("http.proxyPort", proxyPort);
+        // 对https也开启代理
+        System.setProperty("https.proxyHost", proxyHost);
+        System.setProperty("https.proxyPort", proxyPort);
+    
         SpringApplication.run(Application.class, args);
     }
 
